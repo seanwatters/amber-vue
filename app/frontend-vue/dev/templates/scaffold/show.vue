7cff55dae1a7d9277c999dcf09fd57130798019c
github username <sean@watters.io>
2019-10-31 11:46:26 -0700

    Working on templates to be read by CLI

---
+++ app/frontend-vue/dev/templates/scaffold/show.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class=NewComponent>
+    <h3>I am NewComponent!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewComponent',
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .NewComponent {
+
+  }
+  h3 {
+    margin: 40px 0 0;
+    font-weight: lighter;
+  }
+</style>
