046574b5e36650ad846291bc3d66d0d0ab0c40f7
github username <sean@watters.io>
2019-11-01 21:27:06 -0700

    Updated templates to work with CLI 0.2.2

--- app/frontend-vue/dev/templates/scaffold/show.vue
+++ app/frontend-vue/dev/templates/scaffold/show.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class=NewComponent>
-    <h3>I am NewComponent!</h3>
+  <div>
+    <h3>comp_name</h3>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NewComponent',
-
-
+  name: 'comp_nameShow',
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .NewComponent {
-
-  }
   h3 {
     margin: 40px 0 0;
     font-weight: lighter;
